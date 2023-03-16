6ae3c5ec250754f950e81cb9827b02fbdd91ebe8
Vlad <hrinyov@gmail.com>
2023-03-16 23:53:17 +0200

    add Sidebar to App

---
+++ src/views/modules/SidebarLogo.vue
@@ -0,0 +1,25 @@
+<template>
+  <li class="sidebar_logo">
+    <div class="sidebar_logo_items"></div>
+    <div class="sidebar_logo_items"></div>
+    <div class="sidebar_logo_items"></div>
+  </li>
+</template>
+<style>
+.sidebar_logo_items {
+  background-color: #4b1ad3;
+  height: 2px;
+  width: 25px;
+  margin: 5px;
+}
+
+.sidebar_logo {
+  padding: 13px 16px;
+  cursor: pointer;
+  display: block;
+}
+
+.sidebar_logo:hover {
+  background-color: rgb(231, 208, 255);
+}
+</style>
